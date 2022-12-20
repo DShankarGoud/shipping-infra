b63fded172538ac498f24972a49354d58cba9676
shiva shankar <sivas1475@gmail.com>
2022-12-20 23:23:59 +0530

    JENKINS

---
+++ env-dev/backend.tfvars
@@ -0,0 +1,3 @@
+bucket = "terraform-s64"
+key    = "vpc/dev/shipping/terraform.tfstate"
+region = "us-east-1"
---
+++ env-prod/backend.tf
@@ -0,0 +1,3 @@
+bucket = "terraform-s64"
+key    = "vpc/prod/shipping/terraform.tfstate"
+region = "us-east-1"
