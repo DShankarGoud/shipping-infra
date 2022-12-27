a13e3d5e611727c598d7a6eae49b8d1f69fb5e21
shiva shankar <sivas1475@gmail.com>
2022-12-28 02:21:59 +0530

    JENKINS

--- main.tf
+++ main.tf
@@ -6,3 +6,15 @@ module "vpc" {
   SUBNET_CIDR = var.SUBNET_CIDR
   AZ = var.AZ
 }
+
+module "mysql" {
+  depends_on          = [module.vpc]
+  source              = "git::https://github.com/DShankarGoud/tf-module-rds-mysql.git"
+  ENGINE              = var.ENGINE
+  ENGINE_VERSION      = var.ENGINE_VERSION
+  INSTANCE_CLASS      = var.INSTANCE_CLASS
+  NAME                = var.NAME
+  SKIP_FINAL_SNAPSHOT = var.SKIP_FINAL_SNAPSHOT
+  ENV                 = var.ENV
+  COMPONENT           = var.COMPONENT
+}
